f934842c0a089d64f3bfb620ba41fefd7aa33b74
Juanvanegas08 <91501572+Juanvanegas08@users.noreply.github.com>
2024-09-23 00:06:36 -0500

    avance register

--- kulpro/src/app/register/register.page.ts
+++ kulpro/src/app/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private navCtrl: NavController
+  ) { }
 
   ngOnInit() {
+  }
+
+  goBack() {
+    this.navCtrl.back();
   }
 
 }
